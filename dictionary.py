e4ab249a67f39746de5564a8f26dbe107c21117e
vaishnavimishraa <vaishnavii2048@gmail.com>
2025-07-12 22:36:11 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+marks= {
+    "Vaishnavi" : 98,
+    88: "Darsh",
+    "Shreyash" : 78
+
+}
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# (marks.update({"Vaishnavi": 97, "Kanha":89}))
+print(marks.get("Vaishnavi1"))#prints none
+print(marks["Vaishnavi1"])#gives key error
