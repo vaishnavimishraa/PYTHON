7e8a7ee643157524a612344b72886b9653e53dbe
vaishnavimishraa <vaishnavii2048@gmail.com>
2025-07-15 22:32:21 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+a= ["momos", "chat",50, "yummy",52.56, True, "tasty" ]
+a.append("eat")
+print(a)
+
+b=[1,23,65,21,13]
+b.reverse()
+b.sort()
+b.insert(2, 222) #inserting 222 at 2 index position 
+(b.pop(2))
+b.remove(23)
+print(b)
