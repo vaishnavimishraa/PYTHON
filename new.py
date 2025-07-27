93f58442d08893613d88959cf32426f3f1629014
vaishnavimishraa <vaishnavii2048@gmail.com>
2025-07-27 23:03:49 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,11 @@
+def avg():#function definition
+    a= int(input("enter number:"))
+    b= int(input("enter number:"))
+    c= int(input("enter number:"))
+    average=(a+b+c)/3
+    print(average)
+
+avg()#function call
+avg()
+avg()
+avg()
