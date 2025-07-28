320a29da53922da2252c27ed1531f2bc36b15c1b
vaishnavimishraa <vaishnavii2048@gmail.com>
2025-07-28 22:30:40 +0530

    Add files via upload

---
+++ quick_quiz.py
@@ -0,0 +1,10 @@
+# def greet(name, ending):
+#     print("Good day" , name, ending)
+# greet("Yashi","Hello")
+# greet("Kanha","Hello")
+
+def greet(name, ending):
+    print("Good day" , name, ending)
+    return 4
+a= greet("Yashi","Hello")
+print(a)
