8dbdce2e0206ded8b1a98c6c588aa66c23458b49
vaishnavimishraa <vaishnavii2048@gmail.com>
2025-07-30 23:57:21 +0530

    Add files via upload

---
+++ ques1.py
@@ -0,0 +1,14 @@
+def greatest(num1, num2, num3):
+   if(num1>num2 and num1>num3):
+      return num1
+     
+   elif(num2>num1 and num2>num3):
+      return num2
+   else:
+      return num3
+   
+
+
+      
+
+
