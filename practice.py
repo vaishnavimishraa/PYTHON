c526b160aa00ab9454ea19bd97f6c9c12dc98d42
vaishnavimishraa <vaishnavii2048@gmail.com>
2025-10-25 23:45:50 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,10 @@
+def disp(text):
+  return text.upper()
+print(disp ('hello'))
+yell = disp
+print(yell('Hello'))
+def my_function(food):
+    for x in food:
+        print(x)
+fruits = ["apple", "banana", "cherry"]
+my_function(fruits)
