bb27212b13ecbccaf1744415fae7551c4417aea7
vaishnavimishraa <vaishnavii2048@gmail.com>
2025-10-26 23:28:37 +0530

    Add files via upload

---
+++ Second Practical Q12.py
@@ -0,0 +1,8 @@
+#Q12.
+
+a = int(input("Enter a number you want to check whether it's there in sequence or not: "))
+b = [10, 20, 56, 78, 89]
+if a in b:
+    print(a,"is in sequence.")
+else:
+    print(a,"is not in sequence")
