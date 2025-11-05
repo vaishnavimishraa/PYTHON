2421fc6db304b512bb5142f2fce60b0573b0aa60
vaishnavimishraa <vaishnavii2048@gmail.com>
2025-11-05 23:46:13 +0530

    Add files via upload

---
+++ string compare.py
@@ -0,0 +1,18 @@
+a= "Vaishnavi"
+b= "Mishra"
+if a==b:
+    print("they are same.")
+else:
+    print("they are not same.")
+
+#to split and join given string
+
+s= 'Coders are welcome'
+print(s.split(""))
+print("". join(s.split("vaish")))
+    
+
+
+    
+
+    
